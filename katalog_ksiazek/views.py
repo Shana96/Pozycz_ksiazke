2107b9b6f1168ce58acdacd5056957edb2725851
Shana96 <a_lukowska@tlen.pl>
2022-10-23 21:28:44 +0200

    usuniecie widokow

--- katalog_ksiazek/views.py
+++
@@ -1,5 +0,0 @@
-from django.http import HttpResponse
-
-
-def start():
-    return HttpResponse('Hello, world!')
--- uzytkownicy_biblioteki/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class UzytkownicyBibliotekiConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'uzytkownicy_biblioteki'
